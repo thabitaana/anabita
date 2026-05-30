eaf225dda1eda9784863ddfe83b08e74171d9138
Maria Eduarda <mariaeduardapassosleite2009@gmail.com>
2026-05-29 21:23:06 -0400

    Criação de novas telas e bottom navigation

---
+++ app/src/main/java/com/example/lojaderoupas/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.lojaderoupas;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button botao;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        botao = findViewById(R.id.btnLogin);
+
+        botao.setOnClickListener( v->
+                startActivity(new Intent(this, MenuActivity.class))
+        );
+    }
+}
---
+++ app/src/main/java/com/example/lojaderoupas/MenuActivity.java
@@ -0,0 +1,53 @@
+package com.example.lojaderoupas;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.lojaderoupas.databinding.ActivityMainBinding;
+import com.example.lojaderoupas.databinding.ActivityMenuBinding;
+
+public class MenuActivity extends AppCompatActivity {
+
+    ActivityMenuBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMenuBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.home) {
+                replaceFragment(new HomeFragment());
+            } else if (itemId == R.id.carrinho) {
+                replaceFragment(new CarrinhoFragment());
+            } else if (itemId == R.id.perfil) {
+                replaceFragment(new PerfilFragment());
+            } else if (itemId == R.id.config) {
+                replaceFragment(new ConfigFragment());
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+
+    }
+}
